4a5bc14c8f66824e1b596ad2398112f3c1954dab
Mary <marybelka04@gmail.com>
2023-02-19 13:16:03 +0400

    Create function.cpp

---
+++ function.cpp
@@ -0,0 +1,19 @@
+#include <string>
+
+long double fact(int n)
+{
+    if(n < 0) 
+        return 0; 
+    if (n == 0) 
+        return 1; 
+    else 
+        return n * fact(n - 1); 
+};
+long double sqr(int n, float c)
+{
+	return (n*n + c)
+}
+long double min(int n)
+{
+	
+}
