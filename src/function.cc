6ecf5a524b34948b4d3c91b10c24f6eeeddbd239
Mary <marybelka04@gmail.com>
2023-02-23 22:50:02 +0400

    Update function.cc

--- src/function.cc
+++ src/function.cc
@@ -1,4 +1,34 @@
-#include <string>
+#include <function/function.h>
+
+#include <cassert>
+#include <cmath>
+#include <stdexcept>
+
+using namespace function;
+using namespace std;
+
+Function Function::create_sqrc(const int c) {
+    return Function(Type::sqrc, c);
+}
+
+Function Function::create_fack() {
+    return Function(Type::fack);
+}
+
+
+Function::Function() : _type(Type::sqrc), _c(1) { }
+
+Function::Function(const Type type) : _type(Type::fack), _c(0) { }
+
+Function::Function(const Type type, const int c) : _type(Type::sqrc), _c(c) { }
+
+Type Function::get_type() const {
+    return _type;
+}
+
+int Function::get_c() const {
+    return _c;
+}
 
 long double fact(int n)
 {
