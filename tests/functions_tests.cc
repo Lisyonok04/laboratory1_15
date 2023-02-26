7c4acacb83c882ea94c529f90de0477726ea355d
Mary <marybelka04@gmail.com>
2023-02-27 00:26:55 +0400

    Update functions_tests.cc

--- tests/functions_tests.cc
+++ tests/functions_tests.cc
@@ -1,38 +1,30 @@
 #include <gtest/gtest.h>
 
-#include <functions/functions.h>
+#include <function/function.h>
 
-TEST(FunctionsTests, SumStubTest1) {
-    // Arrange
-    int lhs = 2;
-    int rhs = 3;
-    
-    // Act
-    int r = sum_stub(lhs, rhs);
-    
-    // Assert
-    EXPECT_EQ(r, 0);
+using namespace function;
+
+TEST(FunctionTests, Fack_CreateValue) {
+    auto fack1 = Function::create_fack();
+    EXPECT_EQ(fack1.create_fun(5), 120);
+    EXPECT_EQ(fack1.create_fun(2), 2);
+    EXPECT_EQ(fack1.create_fun(1), 1);
+    EXPECT_EQ(fack1.create_fun(0), 1);
+
 }
 
-TEST(FunctionsTests, SumStubTest2) {
-    // Arrange
-    int lhs = -1;
-    int rhs = 3;
-    
-    // Act
-    int r = sum_stub(lhs, rhs);
-    
-    // Assert
-    EXPECT_EQ(r, 36);
-}
-TEST(FunctionsTests, SumStubTest3) {
-    // Arrange
-    int lhs = 7;
-    int rhs = 3;
-    
-    // Act
-    int r = sum_stub(lhs, rhs);
-    
-    // Assert
-    EXPECT_EQ(r, 60);
+
+TEST(FunctionTests, Sqrc_CreateValue) {
+    auto sqrc1 = Function::create_sqrc(1);
+    auto sqrc2 = Function::create_sqrc(3);
+
+    EXPECT_EQ(sqrc1.create_fun(0), 1);
+    EXPECT_EQ(sqrc1.create_fun(2), 5);
+    EXPECT_EQ(sqrc1.create_fun(-3), 10);
+    EXPECT_EQ(sqrc1.create_fun(1), 2);
+
+    EXPECT_EQ(sqrc2.create_fun(0), 3);
+    EXPECT_EQ(sqrc2.create_fun(2), 7);
+    EXPECT_EQ(sqrc2.create_fun(-3), 12);
+    EXPECT_EQ(sqrc2.create_fun(1), 4);
 }
