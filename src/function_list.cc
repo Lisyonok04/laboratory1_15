4a174cbcfdd9a5bac90b2906c960e75df2b2c821
Mary <marybelka04@gmail.com>
2023-02-26 17:45:43 +0400

    Update function_list.cc

--- src/function_list.cc
+++ src/function_list.cc
@@ -31,7 +31,12 @@ void Function_List::remove(int index) {
 }
 
 int Function_List::index_of_min_value(const Function_List& functions, int n) {
-
-
+	int min_index = -1;
+	int min_value = 10000;
+	const auto n = functions.size();
+	for (int i = 0; i < n; i++) {
+		const auto value = functions[i].create_fun(i, c);
+		if 
+	}
 
 }
