1b70353680dd1d97e1bbd7eaee130f5b8b86daaf
Mary <marybelka04@gmail.com>
2023-02-27 09:58:27 +0400

    Update function_list_tests.cc

--- tests/function_list_tests.cc
+++ tests/function_list_tests.cc
@@ -29,6 +29,17 @@ TEST(FunctionListTests, CapacityCheck2) {
 
 }
 
+TEST(FunctionListTests, CapacityCheck3) {
+    Function_List functions;
+    const auto f = Function::create_sqrc(4);
+    for (int i = 0; i < (Function_List::CAPACITY - 1); ++i) {
+        functions.add(f);
+    }
+    functions.insert(4, f);
+    ASSERT_ANY_THROW(functions.insert(4, f));
+
+}
+
 
 TEST(IndexOfFunctionWithMinValue, NoResult) {
     Function_List functions;
@@ -38,7 +49,7 @@ TEST(IndexOfFunctionWithMinValue, NoResult) {
     ASSERT_EQ(index, -1);
 }
 
-TEST(IndexOfFunctionWithMinValue, ArgZero) {
+TEST(IndexOfFunctionWithMinValue, ArgFour1) {
     Function_List functions;
     functions.add(Function::create_sqrc(1));
     functions.add(Function::create_sqrc(2));
@@ -50,14 +61,26 @@ TEST(IndexOfFunctionWithMinValue, ArgZero) {
     ASSERT_EQ(index, 0);
 }
 
+TEST(IndexOfFunctionWithMinValue, ArgFour2) {
+    Function_List functions;
+    functions.add(Function::create_sqrc(1));
+    functions.add(Function::create_fack());
+    functions.add(Function::create_sqrc(2));
+    functions.add(Function::create_fack());
+
+    const auto index = index_of_min_value(functions, 4);
+
+    ASSERT_EQ(index, 0);
+}
+
 TEST(IndexOfFunctionWithMinValue, Arg12) {
     Function_List functions;
-    functions.add(Function::create_sqrc(2));
-    functions.add(Function::create_sqrc(1));
+    functions.add(Function::create_fack());
+    functions.add(Function::create_fack());
     functions.add(Function::create_sqrc(1));
     functions.add(Function::create_sqrc(2));
 
     const auto index = index_of_min_value(functions, 12);
 
-    ASSERT_EQ(index, 1);
+    ASSERT_EQ(index, 2);
 }
