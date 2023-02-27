fc5df4ef42f5b9036253d7870e185a6266adc4c0
Mary <marybelka04@gmail.com>
2023-02-27 08:40:56 +0400

    Update function_list.cc

--- src/function_list.cc
+++ src/function_list.cc
@@ -35,17 +35,20 @@ void Function_List::insert(int index, const Function f) {
 	this -> data[index]._c = f._c;
 }
 
-void Function_List::remove(int index) {
+//void Function_List::remove(int index) {
 
-}
+//}
 
-/*int Function_List::index_of_min_value(const Function_List& functions, int n) {
+int function::index_of_min_value(const Function_List& functions, int x) {
 	int min_index = -1;
 	int min_value = 10000;
 	const auto n = functions.size();
 	for (int i = 0; i < n; i++) {
-		const auto value = functions[i].create_fun(i, _c);
-		if 
+		const auto value = functions[i].create_fun(x);
+		if (min_value > value) {
+			min_index = i;
+			min_value = value;
+		}
 	}
 	return min_index;
-}*/
+}
