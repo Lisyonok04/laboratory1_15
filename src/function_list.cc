ec3806b7374fe205758b923f5c3997fe2019c33d
Mary <marybelka04@gmail.com>
2023-02-27 08:20:12 +0400

    Update function_list.cc

--- src/function_list.cc
+++ src/function_list.cc
@@ -18,25 +18,34 @@ Function Function_List::operator[] (const int index) const{
 	}
 	return data[index];
 }
+void Function_List::add(const Function f) {
+	if (_size == CAPACITY) {
+		throw runtime_error("[FunctionList::add] Capacity is reached.");
+	}
+
+	data[_size] = f;
+	++_size;
+}
 
 void Function_List::insert(int index, const Function f) {
 	if (_size == CAPACITY) {
 		throw runtime_error("[FunctionList::add] Capacity is reached.");
 	}
-
+	this -> data[index]._type = f._type;
+	this -> data[index]._c = f._c;
 }
 
 void Function_List::remove(int index) {
 
 }
 
-int Function_List::index_of_min_value(const Function_List& functions, int n) {
+/*int Function_List::index_of_min_value(const Function_List& functions, int n) {
 	int min_index = -1;
 	int min_value = 10000;
 	const auto n = functions.size();
 	for (int i = 0; i < n; i++) {
-		const auto value = functions[i].create_fun(i, c);
+		const auto value = functions[i].create_fun(i, _c);
 		if 
 	}
 	return min_index;
-}
+}*/
