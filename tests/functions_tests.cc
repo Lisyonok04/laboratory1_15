151f98143a2fd8b52cde690bfbe571de738d2f7c
Mary <marybelka04@gmail.com>
2023-03-27 10:24:53 +0400

    Update functions_tests.cc

--- tests/functions_tests.cc
+++ tests/functions_tests.cc
@@ -6,25 +6,27 @@ using namespace function;
 
 TEST(FunctionTests, Fack_CreateValue) {
     auto fack1 = Function::create_fack();
-    EXPECT_EQ(fack1.create_fun(5), 120);
-    EXPECT_EQ(fack1.create_fun(2), 2);
-    EXPECT_EQ(fack1.create_fun(1), 1);
-    EXPECT_EQ(fack1.create_fun(0), 1);
-
+    EXPECT_EQ(fack1.create_fun(3), 6);
+    auto fack2 = Function::create_fack();
+    EXPECT_EQ(fack2.create_fun(7), 5040);
+    auto fack3 = Function::create_fack();
+    EXPECT_EQ(fack3.create_fun(5), 120);
+    auto fack4 = Function::create_fack();
+    EXPECT_EQ(fack4.create_fun(0), 1);
 }
 
 
 TEST(FunctionTests, Sqrc_CreateValue) {
     auto sqrc1 = Function::create_sqrc(1);
-    auto sqrc2 = Function::create_sqrc(3);
-
-    EXPECT_EQ(sqrc1.create_fun(0), 1);
-    EXPECT_EQ(sqrc1.create_fun(2), 5);
-    EXPECT_EQ(sqrc1.create_fun(-3), 10);
-    EXPECT_EQ(sqrc1.create_fun(1), 2);
-
-    EXPECT_EQ(sqrc2.create_fun(0), 3);
-    EXPECT_EQ(sqrc2.create_fun(2), 7);
-    EXPECT_EQ(sqrc2.create_fun(-3), 12);
-    EXPECT_EQ(sqrc2.create_fun(1), 4);
+    EXPECT_EQ(sqrc1.create_fun(3), 10);
+    auto sqrc2 = Function::create_sqrc(2);
+    EXPECT_EQ(sqrc2.create_fun(4), 18);
+    auto sqrc3 = Function::create_sqrc(0);
+    EXPECT_EQ(sqrc3.create_fun(5), 25);
+    auto sqrc4 = Function::create_sqrc(3);
+    EXPECT_EQ(sqrc4.create_fun(0),3);
+    auto sqrc5 = Function::create_sqrc(-3);
+    EXPECT_EQ(sqrc5.create_fun(5), 22);
+    auto sqrc6 = Function::create_sqrc(4);
+    EXPECT_EQ(sqrc6.create_fun(-2), 8);
 }
