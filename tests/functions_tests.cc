d5893de3627d10b2b5b291e041b476a5759bb139
Mary <marybelka04@gmail.com>
2023-05-04 05:07:18 +0400

    Update functions_tests.cc

--- tests/functions_tests.cc
+++ tests/functions_tests.cc
@@ -4,29 +4,103 @@
 
 using namespace function;
 
-TEST(FunctionTests, Fack_CreateValue) {
-    auto fack1 = Function::create_fack();
-    EXPECT_EQ(fack1.create_fun(3), 6);
-    auto fack2 = Function::create_fack();
-    EXPECT_EQ(fack2.create_fun(7), 5040);
-    auto fack3 = Function::create_fack();
-    EXPECT_EQ(fack3.create_fun(5), 120);
-    auto fack4 = Function::create_fack();
-    EXPECT_EQ(fack4.create_fun(0), 1);
+TEST(FunctionTests, Fack_CreateValue1) {
+
+    Function s(Type::fack);
+    int ans = s.create_fun(3);
+    EXPECT_EQ(ans, 6);
+
 }
 
+TEST(FunctionTests, Fack_CreateValue2) {
 
-TEST(FunctionTests, Sqrc_CreateValue) {
-    auto sqrc1 = Function::create_sqrc(1);
-    EXPECT_EQ(sqrc1.create_fun(3), 10);
-    auto sqrc2 = Function::create_sqrc(2);
-    EXPECT_EQ(sqrc2.create_fun(4), 18);
-    auto sqrc3 = Function::create_sqrc(0);
-    EXPECT_EQ(sqrc3.create_fun(5), 25);
-    auto sqrc4 = Function::create_sqrc(3);
-    EXPECT_EQ(sqrc4.create_fun(0),3);
-    auto sqrc5 = Function::create_sqrc(-3);
-    EXPECT_EQ(sqrc5.create_fun(5), 22);
-    auto sqrc6 = Function::create_sqrc(4);
-    EXPECT_EQ(sqrc6.create_fun(-2), 8);
+    Function s(Type::fack);
+    int ans = s.create_fun(0);
+    EXPECT_EQ(ans, 1);
+
+}
+
+TEST(FunctionTests, Fack_CreateValue3) {
+
+    Function s(Type::fack);
+    int ans = s.create_fun(4);
+    EXPECT_EQ(ans, 24);
+
+}
+
+TEST(FunctionTests, Fack_CreateValue4) {
+
+    Function s(Type::fack);
+    int ans = s.create_fun(5);
+    EXPECT_EQ(ans, 120);
+
+}
+
+TEST(FunctionTests, Fack_CreateValue5) {
+
+    Function s(Type::fack);
+    int ans = s.create_fun(1);
+    EXPECT_EQ(ans, 1);
+
+}
+
+TEST(FunctionTests, Sqrc_CreateValue1) {
+    Function s(Type::sqrc, 3);
+    int ans1 = s.create_fun(1);
+    int ans2 = s.create_fun(4);
+    int ans3 = s.create_fun(5);
+
+    EXPECT_EQ(ans1, 3);
+    EXPECT_EQ(ans2, 19);
+    EXPECT_EQ(ans3, 28);
+}
+
+TEST(FunctionTests, Sqrc_CreateValue2) {
+    Function s(Type::sqrc, 0);
+    int ans1 = s.create_fun(2);
+    int ans2 = s.create_fun(7);
+    int ans3 = s.create_fun(10);
+
+    EXPECT_EQ(ans1, 4);
+    EXPECT_EQ(ans2, 49);
+    EXPECT_EQ(ans3, 100);
+}
+
+TEST(FunctionTests, Sqrc_CreateValue3) {
+    Function s(Type::sqrc, -4);
+    int ans1 = s.create_fun(3);
+    int ans2 = s.create_fun(6);
+    int ans3 = s.create_fun(9);
+
+    EXPECT_EQ(ans1, 5);
+    EXPECT_EQ(ans2, 32);
+    EXPECT_EQ(ans3, 77);
+}
+
+TEST(FunctionTests, Sqrc_CreateValueSetShift1) {
+    Function s(Type::sqrc, 5);
+    s.set_c(4);
+
+    int ans1 = s.create_fun(1);
+    int ans2 = s.create_fun(4);
+    int ans3 = s.create_fun(5);
+
+    EXPECT_EQ(s.get_c(), 4);
+    EXPECT_EQ(ans1, 4);
+    EXPECT_EQ(ans3, 20);
+    EXPECT_EQ(ans3, 29);
+}
+
+TEST(FunctionTests, Sqrc_CreateValueSetShift2) {
+    Function s(Type::sqrc, -1);
+    s.set_c(0);
+
+    int ans1 = s.create_fun(2);
+    int ans2 = s.create_fun(3);
+    int ans3 = s.create_fun(6);
+
+    EXPECT_EQ(s.get_c(), 0);
+    EXPECT_EQ(ans1, 4);
+    EXPECT_EQ(ans3, 9);
+    EXPECT_EQ(ans3, 36);
 }
