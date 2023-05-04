4a9c218d2bb4696759c65129df229a3c72156fd5
Mary <marybelka04@gmail.com>
2023-05-04 08:26:49 +0400

    Update function_list_tests.cc

--- tests/function_list_tests.cc
+++ tests/function_list_tests.cc
@@ -5,169 +5,185 @@
 using namespace function;
 
 TEST(FunctionListTests, CapacityCheck) {
-    Function_List sequence;
-
-    const auto s = Function::Function(Type::sqrc, 0);
-    for (int i = 0; i < Function_List::capacity; ++i) {
-        sequence.insert(i, s);
-    }
-    EXPECT_ANY_THROW(sequence.insert(10, s), std::runtime_error);
-}
-
-TEST(FunctionListTests, CapacityCheck2) {
-    Function_List sequence;
-
-    const auto s = Function::Function(Type::sqrc, 0);
-    for (int i = 0; i < Function_List::capacity; ++i) {
-        sequence.insert(i, s);
-    }
-    EXPECT_NO_THROW(sequence.insert(10, s), std::runtime_error);
-}
-
-TEST(FunctionListTests, AddingCheck) {
-    Function_List sequence;
-
-    sequence.insert(0, Function::Function(Type::sqrc, 3));
-    sequence.insert(1, Function::Function(Type::fack));
-    sequence.insert(2, Function::Function(Type::sqrc, 3));
-    sequence.insert(3, Function::Function(Type::sqrc, 4));
-    sequence.add(1, Function::Function(Type::sqrc, 1));
-    auto index = index_of_min_value(sequence, 2);
-
-    ASSERT_EQ(index, 1);
+    Function_List sequence(3);
+    sequence[0] = Function(Type::sqrc, 3);
+    sequence[1] = Function(Type::sqrc, 2);
+    sequence[2] = Function(Type::sqrc, 5);
+    Function s = Function(Type::sqrc, 4);
+    ASSERT_ANY_THROW(sequence.insert(4, s););
 }
 
 TEST(IndexOfSequenceWithMinValue, NoResult) {
     Function_List sequence;
 
-    auto index = index_of_min_value(sequence, 2);
+    const auto index = index_of_min_value(sequence, 2);
 
     ASSERT_EQ(index, -1);
 }
 
 TEST(IndexOfSequenceWithMinValue, ForSquares1) {
     Function_List sequence;
+    Function s = Function(Type::sqrc, 0);
+    Function s1 = Function(Type::sqrc, 1);
+    sequence.insert(0, s);
+    sequence.insert(1, s1);
+    sequence.insert(2, s1);
+    sequence.insert(3, s);
 
-    sequence.insert(0, Function::Function(Type::sqrc, 0));
-    sequence.insert(1, Function::Function(Type::sqrc, 1));
-    sequence.insert(2, Function::Function(Type::sqrc, 1));
-    sequence.insert(3, Function::Function(Type::sqrc, 0));
-
-    auto index = index_of_min_value(sequence, 0);
+    const auto index = index_of_min_value(sequence, 0);
 
     ASSERT_EQ(index, 3);
 }
 
 TEST(IndexOfSequenceWithMinValue, ForSquares2) {
     Function_List sequence;
+    Function s = Function(Type::sqrc, 1);
+    Function s1 = Function(Type::sqrc, 2);
+    Function s2 = Function(Type::sqrc, 3);
+    Function s3 = Function(Type::sqrc, 4);
+    sequence.insert(0, s);
+    sequence.insert(1, s1);
+    sequence.insert(2, s2);
+    sequence.insert(3, s3);
 
-    sequence.insert(0, Function::Function(Type::sqrc, 1));
-    sequence.insert(1, Function::Function(Type::sqrc, 2));
-    sequence.insert(2, Function::Function(Type::sqrc, 3));
-    sequence.insert(3, Function::Function(Type::sqrc, 4));
+    const auto index = index_of_min_value(sequence, 5);
 
-    auto index = index_of_min_value(sequence, 5);
+    ASSERT_EQ(index, 0);
+}
+
+TEST(IndexOfSequenceWithMinValue, ForSquares3) {
+    Function_List sequence;
+    Function s = Function(Type::sqrc, 1);
+    Function s1 = Function(Type::sqrc, 2);
+    Function s2 = Function(Type::sqrc, 3);
+    Function s3 = Function(Type::sqrc, 4);
+    sequence.insert(0, s);
+    sequence.insert(1, s1);
+    sequence.insert(2, s2);
+    sequence.insert(3, s3);
+
+    const auto index = index_of_min_value(sequence, 5);
 
     ASSERT_EQ(index, 0);
 }
 
 TEST(IndexOfSequenceWithMinValue, ForFactorials1) {
     Function_List sequence;
+    Function s = Function(Type::fack);
+    Function s1 = Function(Type::fack);
+    Function s2 = Function(Type::fack);
+    Function s3 = Function(Type::fack);
+    sequence.insert(0, s);
+    sequence.insert(1, s1);
+    sequence.insert(2, s2);
+    sequence.insert(3, s3);
 
-    sequence.insert(0, Function::Function(Type::fack));
-    sequence.insert(1, Function::Function(Type::fack));
-    sequence.insert(2, Function::Function(Type::fack));
-    sequence.insert(3, Function::Function(Type::fack));
-
-    auto index = index_of_min_value(sequence, 0);
+    const auto index = index_of_min_value(sequence, 0);
 
     ASSERT_EQ(index, 3);
 }
 
 TEST(IndexOfSequenceWithMinValue, ForFactorials2) {
     Function_List sequence;
+    Function s = Function(Type::fack);
+    Function s1 = Function(Type::fack);
+    Function s2 = Function(Type::fack);
+    Function s3 = Function(Type::fack);
+    sequence.insert(0, s);
+    sequence.insert(1, s1);
+    sequence.insert(2, s2);
+    sequence.insert(3, s3);
 
-    sequence.insert(0, Function::Function(Type::fack));
-    sequence.insert(1, Function::Function(Type::fack));
-    sequence.insert(2, Function::Function(Type::fack));
-    sequence.insert(3, Function::Function(Type::fack));
-
-    auto index = index_of_min_value(sequence, 5);
+    const auto index = index_of_min_value(sequence, 5);
 
     ASSERT_EQ(index, 3);
 }
 
 TEST(IndexOfSequenceWithMinValue, Mix1) {
     Function_List sequence;
+    Function s = Function(Type::fack);
+    Function s1 = Function(Type::fack);
+    Function s2 = Function(Type::sqrc, 3);
+    Function s3 = Function(Type::fack);
+    Function s4 = Function(Type::fack);
+    sequence.insert(0, s);
+    sequence.insert(1, s1);
+    sequence.insert(2, s2);
+    sequence.insert(3, s3);
+    sequence.insert(4, s4);
 
-    sequence.insert(0, Function::Function(Type::fack));
-    sequence.insert(1, Function::Function(Type::sqrc, 3));
-    sequence.insert(2, Function::Function(Type::fack));
-    sequence.insert(3, Function::Function(Type::sqrc, 0));
+    const auto index = index_of_min_value(sequence, 0);
 
-    auto index = index_of_min_value(sequence, 5);
-
-    ASSERT_EQ(index, 3);
+    ASSERT_EQ(index, 4);
 }
 
 TEST(IndexOfSequenceWithMinValue, Mix2) {
     Function_List sequence;
+    Function s = Function(Type::sqrc, 0);
+    Function s1 = Function(Type::fack);
+    Function s2 = Function(Type::sqrc, 3);
+    Function s3 = Function(Type::sqrc, 4);
+    sequence.insert(0, s);
+    sequence.insert(1, s1);
+    sequence.insert(2, s2);
+    sequence.insert(3, s3);
 
-    sequence.insert(0, Function::Function(Type::sqrc, 0));
-    sequence.insert(1, Function::Function(Type::fack));
-    sequence.insert(2, Function::Function(Type::sqrc, 3));
-    sequence.insert(3, Function::Function(Type::sqrc, 4));
-
-    auto index = index_of_min_value(sequence, 1);
+    const auto index = index_of_min_value(sequence, 1);
 
     ASSERT_EQ(index, 1);
 }
 
 TEST(IndexOfSequenceWithMinValue, Mix3) {
     Function_List sequence;
+    Function s = Function(Type::fack);
+    Function s1 = Function(Type::fack);
+    Function s2 = Function(Type::sqrc, 1);
+    Function s3 = Function(Type::fack);
+    Function s4 = Function(Type::sqrc, 3);
+    Function s5 = Function(Type::sqrc, 5);
+    Function s6 = Function(Type::fack);
+    Function s7 = Function(Type::sqrc, 1);
+    Function s8 = Function(Type::sqrc, 7);
+    Function s9 = Function(Type::fack);
+    sequence.insert(0, s);
+    sequence.insert(1, s1);
+    sequence.insert(2, s2);
+    sequence.insert(3, s3);
+    sequence.insert(4, s4);
+    sequence.insert(5, s5);
+    sequence.insert(6, s6);
+    sequence.insert(7, s7);
+    sequence.insert(8, s8);
+    sequence.insert(9, s9);
 
-    sequence.insert(0, Function::Function(Type::fack));
-    sequence.insert(1, Function::Function(Type::sqrc, 2));
-    sequence.insert(2, Function::Function(Type::sqrc, 1));
-    sequence.insert(3, Function::Function(Type::sqrc, 3));
 
-    auto index = index_of_min_value(sequence, 5);
-
-    ASSERT_EQ(index, 2);
-}
-
-TEST(IndexOfSequenceWithMinValue, Mix4) {
-    Function_List sequence;
-
-    sequence.insert(0, Function::Function(Type::fack));
-    sequence.insert(1, Function::Function(Type::fack));
-    sequence.insert(2, Function::Function(Type::sqrc, 1));
-    sequence.insert(3, Function::Function(Type::fack));
-    sequence.insert(4, Function::Function(Type::sqrc, 3));
-    sequence.insert(5, Function::Function(Type::sqrc, 5));
-    sequence.insert(6, Function::Function(Type::fack));
-    sequence.insert(7, Function::Function(Type::sqrc, 1));
-    sequence.insert(8, Function::Function(Type::sqrc, 7));
-    sequence.insert(9, Function::Function(Type::fack));
-
-    auto index = index_of_min_value(sequence, 5);
+    const auto index = index_of_min_value(sequence, 5);
 
     ASSERT_EQ(index, 7);
 }
 
-TEST(IndexOfSequenceWithMinValue, Mix5) {
+TEST(IndexOfSequenceWithMinValue, Mix4) {
     Function_List sequence;
-
-    sequence.insert(0, Function::Function(Type::fack));
-    sequence.insert(1, Function::Function(Type::fack));
-    sequence.insert(2, Function::Function(Type::sqrc, 4));
-    sequence.insert(3, Function::Function(Type::fack));
-    sequence.insert(4, Function::Function(Type::sqrc, 3));
-    sequence.insert(5, Function::Function(Type::sqrc, 5));
-    sequence.insert(6, Function::Function(Type::fack));
-    sequence.insert(7, Function::Function(Type::sqrc, 8));
-    sequence.insert(8, Function::Function(Type::sqrc, 7));
-    sequence.insert(9, Function::Function(Type::fack));
+    Function s = Function(Type::fack);
+    Function s1 = Function(Type::fack);
+    Function s2 = Function(Type::sqrc, 4);
+    Function s3 = Function(Type::fack);
+    Function s4 = Function(Type::sqrc, 3);
+    Function s5 = Function(Type::sqrc, 5);
+    Function s6 = Function(Type::fack);
+    Function s7 = Function(Type::sqrc, 8);
+    Function s8 = Function(Type::sqrc, 7);
+    Function s9 = Function(Type::fack);
+    sequence.insert(0, s);
+    sequence.insert(1, s1);
+    sequence.insert(2, s2);
+    sequence.insert(3, s3);
+    sequence.insert(4, s4);
+    sequence.insert(5, s5);
+    sequence.insert(6, s6);
+    sequence.insert(7, s7);
+    sequence.insert(8, s8);
+    sequence.insert(9, s9);
 
     auto index = index_of_min_value(sequence, 1);
 
